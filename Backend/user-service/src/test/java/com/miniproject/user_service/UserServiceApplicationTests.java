da09f82a3e52998c51aba8ee6728912aa891b96b
10Pratik01 <using0this@gmail.com>
2026-04-20 04:10:41 +0530

    feat: initialize microservices backend architecture with Maven multi-module project structure

---
+++ Backend/user-service/src/test/java/com/miniproject/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproject.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
